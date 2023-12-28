366df4c90d03bd9e0089e8bcd990a1b1a89bec4b
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-12-28 17:57:57 -0300

    Configurado proyecto inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <NavBar />
+    
   );
 }
 
